a6ff4e2983ef902e9ca952366dc6ae25c95b60cb
mxr <chengzhang544@gmail.com>
2023-03-22 12:06:36 -0400

    init

---
+++ include/llop/AST/Expr/Expr.h
@@ -0,0 +1,17 @@
+//
+// Created by mbp on 2023/3/22.
+//
+
+#ifndef LLVM_OOP_EXPR_H
+#define LLVM_OOP_EXPR_H
+
+
+#include "../Node.h"
+
+class Expr : public Node {
+
+};
+
+#endif //LLVM_OOP_EXPR_H
+
+
---
+++ include/llop/AST/Node.h
@@ -0,0 +1,12 @@
+//
+// Created by mbp on 2023/3/22.
+//
+
+#ifndef LLVM_OOP_NODE_H
+#define LLVM_OOP_NODE_H
+
+class Node {
+
+};
+
+#endif //LLVM_OOP_NODE_H
